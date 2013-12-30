73ade70429c50fb9e4709d4b06b27e66cee47ec1
Nando-bog <fernando.diazdelcastillo@gmail.com>
2013-12-30 02:58:58 -0500

    Primer borrador de modelos completo.

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,15 +1,74 @@
+#coding=utf-8
 from django.contrib import admin
-from cursos.models import Curso, Leccion, Recurso
+from cursos.models import Recurso, HiloConductor, TopicoGenerativo, MetaDeComprension, DesempenoDeComprension, Curso, HilosConductoresCurso, TopicosGenerativosCurso, MetasDeComprensionCurso, DesempenosCurso
 
-class CursoAdmin(admin.ModelAdmin):
-    list_display = ('codigo', 'nombre', 'imagen_destacada')
-    filter_horizontal = ['profesor',]
+class HilosConductoresCursoInline(admin.TabularInline):
+    model = HilosConductoresCurso
+    extra = 1
     
+class TopicosGenerativosCursoInline(admin.TabularInline):
+    model = TopicosGenerativosCurso
+    extra = 1
+    
+class MetasDeComprensionCursoInline(admin.TabularInline):
+    model = MetasDeComprensionCurso
+    extra = 1
+
+class DesempenosCursoInline(admin.TabularInline):
+    model = DesempenosCurso
+    extra = 1
+
 class RecursoAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'nombre_corto', 'tipo', 'tags')
-    filter_horizontal = ['autor']
+    list_display = ('nombre', 'nombre_corto', 'adjunto', 'url', 'tipo')
+    filter_horizontal = ['autor',]
+    prepopulated_fields = {'nombre_corto': ('nombre',)}
+    fieldsets = (
+        (None, {'fields': ('nombre', 'nombre_corto', 'url', 'tipo', 'adjunto', 'html', 'autor')}),
+    )
+
+class HiloConductorAdmin(admin.ModelAdmin):
+    list_display=('nombre_corto', 'nombre')
     prepopulated_fields = {'nombre_corto': ('nombre',)}
 
-admin.site.register(Curso, CursoAdmin)
+class TopicoGenerativoAdmin(admin.ModelAdmin):
+    list_display=('nombre_corto', 'nombre')
+    prepopulated_fields = {'nombre_corto': ('nombre',)}
+
+class MetaDeComprensionAdmin(admin.ModelAdmin):
+    list_display=('nombre_corto', 'nombre')
+    prepopulated_fields = {'nombre_corto': ('nombre',)}
+
+class DesempenoDeComprensionAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ("Información básica", {'fields': ('nombre', 'duracion',)}),
+        ("Cuerpo", {'fields': ('cuerpo',)}),
+        ("Recursos", {'fields': ('recursos',),'classes': ('collapse',)}),
+        ("Metadatos", {'fields': ( 'autor', 'fecha_publicacion', 'fecha_actualizacion', 'tags', 'notas_profesor' ), 'classes' : ('collapse', )}),
+    )
+    inlines = (DesempenosCursoInline,)
+    #fieldsets= (
+    #    ("Información básica", {'fields': ('nombre', 'duracion', 'autor')}),
+    #    ('Recursos', {'fields': ('recursos'), 'classes': ('collapse',)}),
+    #    ('Cuerpo', {'fields': ('html',)}),
+    #    ('Meta', {'fields': ('fecha_publicacion', 'fecha_actualizacion', 'tags', 'notas_profesor')})
+    #)
+    
+    filter_horizontal=['autor', 'recursos',]
+
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'nombre')
+    filter_horizontal = ['profesores', 'inscritos', 'hilos_conductores', 'topicos_generativos', 'metas_de_comprension',]
+    inlines = (HilosConductoresCursoInline, TopicosGenerativosCursoInline, MetasDeComprensionCursoInline, DesempenosCursoInline )
+    #fieldsets = (
+    #    ("Información´ básica", {'fields':('codigo', 'nombre', 'acceso', 'cupos', 'imagen_destacada',)}),
+    #    ('Profesores', {'fields':('profesores',)}),
+    #    ('Inscritos', {'fields':('inscritos',)}),
+    #    ("Diseño curricular", {'fields':('hilos_conductores','topicos_generativos', 'metas_de_comprension',)}),        
+    #)
+
 admin.site.register(Recurso, RecursoAdmin)
-admin.site.register(Leccion)
+admin.site.register(HiloConductor, HiloConductorAdmin)
+admin.site.register(TopicoGenerativo, TopicoGenerativoAdmin)
+admin.site.register(MetaDeComprension, MetaDeComprensionAdmin)
+admin.site.register(DesempenoDeComprension, DesempenoDeComprensionAdmin)
+admin.site.register(Curso, CursoAdmin)
--- cursos/models.py
+++ cursos/models.py
@@ -1,15 +1,319 @@
 # coding=utf-8
-# Modelos para la aplicación de Cursos
+# Modelos para la aplicación "Cursos".
 
-# Standard Python/Django library imports
+# Standard Python/Django libraries
 import datetime
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
 from django.db import models
-# Third party library imports
+# Third party libraries
 from ckeditor.fields import RichTextField
 from taggit.managers import TaggableManager
-# Local library imports
+# Local libraries
+# None yet
+
+class Recurso(models.Model):
+    """
+    Representa cualquier tipo de recurso de aprendizaje que se puede utilizar como apoyo a un desempeño o ser independiente, como una entrada de blog-tutorial o una biblioteca de recursos comentados.
+    """
+    #Constantes
+    VIDEO='VID'
+    AUDIO='AUD'
+    IMAGEN='IMG'
+    TEXTO='TEX'
+    MULTIMEDIA='MLT'
+    TIPOS_RECURSO = (
+        (VIDEO, 'Video'),
+        (AUDIO, 'Audio'),
+        (IMAGEN, 'Imagen'),
+        (TEXTO, 'Texto'),
+        (MULTIMEDIA, 'Multimedia'),
+    )
+    #Atributos básicos y obligatorios
+    autor = models.ManyToManyField(User)
+    nombre = models.CharField(
+        max_length=150,
+        help_text="Ojo con la ortografía de los títulos en español. Máximo 150 caracteres."
+        )
+    html = RichTextField(
+        help_text="Descripción o instrucciones del recurso si es un adjunto o URL; contenido completo si es un texto o multimedia para deplegar en línea en la lección. Puede ser HTML."
+        )
+    tipo = models.CharField(
+        max_length=5,
+        choices=TIPOS_RECURSO
+        )
+    #Atributos opcionales
+    adjunto = models.FileField(
+        upload_to='recurso_adjunto',
+        blank=True, help_text="Adjunto que se guardará con el recurso, como una imágen, PDF, video, etc."
+        )
+    url = models.URLField(
+        blank=True,
+        help_text="URL del recurso si proviene de otro sitio, como Youtube, Slideshare, etc."
+        )
+    #Metadatos opcionales
+    nombre_corto=models.CharField(
+        max_length=30,
+        blank=True,
+        help_text="Nombre corto que se verá en listas y otros lugares. Debe ser legible. Máximo 30 caracteres."
+        )
+    tags = TaggableManager(help_text="Lista de tags separados por comas.")
+    
+    class Meta:
+        ordering=['nombre']
+        verbose_name_plural='Recursos'
+        
+    class Admin:
+        pass
+    
+    def __unicode__(self):
+        if self.nombre_corto:
+            return self.nombre_corto
+        return self.nombre
+    
+    def get_absolute_url(self):
+        return "recursos/%s/%s/" % (self.tipo, self.id )
+
+#Atributos pedagógicos de los cursos. Inspirado ligeramente en Collaborative Curriculum Design Tool ofrecido por Wide World en https://learnweb.harvard.edu/ccdt/index.cfm y las ideas sobre diseño curricular, EpC/TFU, de los investigadores de Proyecto Cero en la Escuela Posgragos en Educación de la Universidad de Harvard.
+
+class HiloConductor(models.Model):
+    nombre=RichTextField(help_text="Las metas de comprensión de nivel superior que abarcan todo el proceso.")
+    nombre_corto=models.CharField(
+        max_length=30,
+        blank=True,
+        help_text="Nombre corto que se verá en listas y otros lugares. Debe ser legible. Máximo 30 caracteres. Use ortografía de título."
+    )
+    
+    class Meta:
+        ordering=['nombre']
+        verbose_name_plural='Hilos conductores'
+        verbose_name='Hilo conductor'
+    
+    class Admin:
+        pass
+    
+    def __unicode__(self):
+        if self.nombre_corto:
+            return self.nombre_corto
+        return self.nombre
+    
+    def get_absolute_url(self):
+        return "hilos/%s/" % self.id
+        
+
+class TopicoGenerativo(models.Model):
+    nombre=RichTextField(help_text="Un tópico generativo es una idea compleja, profunda, que se conecta con otras ideas o conceptos importantes de la disciplina. Guía el diseño de una unidad curricular, como un curso.")
+    nombre_corto=models.CharField(
+        max_length=30,
+        blank=True,
+        help_text="Nombre corto que se verá en listas y otros lugares. Debe ser legible. Máximo 30 caracteres. Use ortografía de título."
+    )
+        
+    class Meta:
+        ordering=['nombre']
+        verbose_name="Tópico generativo"
+        verbose_name_plural="Tópicos generativos"
+    
+    class Admin:
+        pass
+    
+    def __unicode__(self):
+        if self.nombre_corto:
+            return self.nombre_corto
+        return self.nombre
+    
+    def get_absolute_url(self):
+        return "topicos/%s/" % self.id    
+
+class MetaDeComprension(models.Model):
+    nombre=RichTextField(help_text="Una meta de comprensión identifica lo que se busca que lo estudiantes logren saber y saber hacer. Puede ser un enunciado o una pregunta. P. Ej. Los estudiantes comprenderán cómo utilizar la sierra sinfín de manera segura; o ¿Cómo operar la sierra sinfín de manera segura?")
+    nombre_corto=models.CharField(
+        max_length=30,
+        blank=True,
+        help_text="Nombre corto que se verá en listas y otros lugares. Debe ser legible. Máximo 30 caracteres. Use ortografía de título."
+        )
+        
+    class Meta:
+        ordering=['nombre']
+        verbose_name="Meta de comprensión"
+        verbose_name_plural="Metas de comprensión"
+    
+    class Admin:
+        pass
+    
+    def __unicode__(self):
+        if self.nombre_corto:
+            return self.nombre_corto
+        return self.nombre
+    
+    def get_absolute_url(self):
+        return "metas/%s/" % self.id 
+
+class DesempenoDeComprension(models.Model):
+    """
+    Un desempeño es la unidad de contenido de un curso o proyecto. Una secuencia de desempeños progresivamente más complejos representa la secuencia básica de un curso. Cada desempeño que busca avanzar hacia el logro de una o más metas de comprensión. Puede ser parte de una secuencia para conformar un curso o proyecto o ser independiente.
+    """
+    #Atributos básicos y obligatorios
+    autor = models.ManyToManyField(
+        User,
+        help_text="Autor de esta lección."
+        )
+    nombre = models.CharField(
+        max_length=150,
+        help_text="Ojo con la ortografía de los títulos en español. Máximo 150 caracteres."
+        )
+    fecha_publicacion = models.DateField()
+    fecha_actualizacion = models.DateField()
+    recursos = models.ManyToManyField(
+        Recurso,
+        help_text="Recursos que se utilizan para apoyar este desempeño. Aparecen adjuntos al texto HTML.",
+        blank=True
+        )
+    cuerpo = RichTextField(
+        blank=True,
+        help_text="Cuerpo de la lección que se deplegará para el estudiante."
+        )
+    #Metadatos opcionales
+    tags = TaggableManager(help_text="Lista de tags separados por comas.")
+    notas_profesor = RichTextField(
+        blank=True,
+        help_text="Notas para el profesor.")
+    duracion = models.IntegerField(
+        help_text="Tiempo requerido para lograr este desempeño en minutos.",
+        verbose_name="Duración"
+        )
+    
+    class Meta:
+        ordering=['nombre']
+        verbose_name="Desempeño de comprensión"
+        verbose_name_plural="Desempeños de comprensión"
+    
+    class Admin:
+        pass
+    
+    def __unicode__(self):
+        return self.nombre
+    
+    def get_absolute_url(self):
+        return "leccion/%s/" % self.id 
+
+
+class Curso(models.Model):
+    """
+    Un curso está definido por un conjunto de tópicos generativos, metas de comprensión, desempeños de comprensión y recursos. Un proyecto es un tipo de curso.
+    """
+    #Constantes
+    ABIERTO_LIBRE = 'AB-L'
+    ABIERTO_CON_INSCRIPCION = 'AB-I'
+    PRIVADO = 'PR'
+    VIP = 'VIP'
+    TIPOS_ACCESO = (
+        (ABIERTO_LIBRE,"Abierto y libre"),
+        (ABIERTO_CON_INSCRIPCION,"Abierto con inscripción"),
+        (PRIVADO,"Exclusivo para miembros"),
+        (VIP,"Requiere autorización especial")
+    )
+    
+    #Atributos básicos y obligatorios
+    codigo=models.CharField(
+        max_length=16,
+        unique=True,
+        validators=[RegexValidator(regex='^[A-Z]{1,5}-\d{1,4}-?[a-z]??',
+        message="No es un código de curso válido. Intente de nuevo. Los códigos válidos se ajustan a la expresión regular anterior.")],
+        help_text="Código del curso. Debe ser único y de máximo 12 caracteres. Solo puede contener números, letras y guiones así: \^[A-Z]{1,5}-\\d{1,4}-?[a-z]??$"
+        )
+    nombre=models.CharField(
+        max_length=150,
+        help_text="Ojo con la ortografía de los títulos en español. Máximo 150 caracteres."
+        )
+    profesores = models.ManyToManyField(
+        User,
+        related_name='profesores',
+        help_text="Autores y profesores del curso. Debe haber al menos uno."
+        )
+    acceso = models.CharField(
+        max_length=5,
+        choices=TIPOS_ACCESO,
+        default = ABIERTO_LIBRE
+        )
+    cupos = models.IntegerField(
+        blank=True,
+        null=True,
+        help_text="Cupo máximo del curso."
+        )
+    inscritos = models.ManyToManyField(
+        User,
+        related_name='inscritos',
+        blank=True,
+        help_text="Agregar alumnos a este curso."
+        )
+    
+    #Atributos de diseño curricular (también obligatorios)
+    hilos_conductores = models.ManyToManyField(
+        HiloConductor,
+        through='HilosConductoresCurso'
+        )
+    topicos_generativos = models.ManyToManyField(
+        TopicoGenerativo,
+        through='TopicosGenerativosCurso'
+        )
+    metas_de_comprension = models.ManyToManyField(
+        MetaDeComprension,
+        through='MetasDeComprensionCurso'
+        )
+    desempenos_de_comprension = models.ManyToManyField(
+        DesempenoDeComprension,
+        through='DesempenosCurso'
+    )
+    #Atributos opcionales
+    imagen_destacada=models.ImageField(
+        upload_to='curso_imagen_destacada',
+        blank=True
+        )
+    #Metadatos
+    tags = TaggableManager(help_text="Lista de tags separados por comas.")
+    
+    class Meta:
+        verbose_name_plural="Cursos"
+        ordering=['nombre']
+    
+    class Admin:
+        pass
+    
+    def __unicode__(self):
+        return self.nombre
+    
+    def get_absolute_url(self):
+        return "cursos/%s/" % self.codigo
+
+class HilosConductoresCurso(models.Model):
+    """
+    Contiene la relación entre Hilos conductores y cursos, junto a información adicional sobre la relación.
+    """
+    hilo_conductor=models.ForeignKey(HiloConductor)
+    curso=models.ForeignKey(Curso)
+
+class TopicosGenerativosCurso(models.Model):
+    """
+    Contiene la relación entre Tópicos generativos y cursos, junto a información adicional sobre la relación.
+    """
+    topico_generativo=models.ForeignKey(TopicoGenerativo)
+    curso=models.ForeignKey(Curso)
+    
+class MetasDeComprensionCurso(models.Model):
+    """
+    Contiene la relación entre Metas de comprensión y cursos, junto a información adicional sobre la relación.
+    """
+    meta_de_comprension=models.ForeignKey(MetaDeComprension)
+    curso=models.ForeignKey(Curso)
+
+class DesempenosCurso(models.Model):
+    """
+    Contiene la relación entre Desempeños y cursos, junto a información adicional sobre el desempeño en ese curso.
+    """
+    desempeno=models.ForeignKey(DesempenoDeComprension)
+    curso=models.ForeignKey(Curso)
+    orden=models.IntegerField()
 
 #class Categoria(models.Model):
 #    nombre = models.CharField(max_length=150, help_text="Ojo con la ortografía de los títulos en español. Máximo 150 caracteres.")
@@ -27,88 +331,4 @@ from taggit.managers import TaggableManager
 #        return self.nombre
 #    
 #    def get_absolute_url(self):
-#        return "cat/%s/" % self.slug  
-
-class Curso(models.Model):
-    """
-    Un curso es una colección de contenidos ordenados con un criterio didáctico. Un proyecto es un tipo de curso enfocado en la práctica mediante la creación de un artefacto.
-    """
-    TIPOS_ACCESO = (
-        ('AB-L',"Abierto y libre"),
-        ('AB-I',"Abierto con inscripción"),
-        ('CE',"Cerrado"),
-    )
-    #Atributos básicos y obligatorios
-    codigo=models.CharField(max_length=16, unique=True, validators=[RegexValidator(regex='^[A-Z]{1,5}-\d{1,4}-?[a-z]??', message="No es un código de curso válido. Intente de nuevo. Los códigos válidos se ajustan a la expresión regular anterior.")], help_text="Código del curso. Debe ser único y de máximo 12 caracteres. Solo puede contener números, letras y guiones así: \^[A-Z]{1,5}-\\d{1,4}-?[a-z]??$")
-    nombre=models.CharField(max_length=150, help_text="Ojo con la ortografía de los títulos en español. Máximo 150 caracteres.")
-    profesor = models.ManyToManyField(User, help_text="Autores y profesores del curso. Debe haber al menos uno.")
-    acceso = models.CharField(max_length=5, choices=TIPOS_ACCESO)
-    #Atributos opcionales
-    imagen_destacada=models.ImageField(upload_to='curso_imagen_destacada', blank=True)
-    #Meta datos
-    tags = TaggableManager(help_text="Lista de tagas separados por comas.")
-    
-    class Meta:
-        verbose_name_plural="Cursos"
-        ordering=['nombre']
-    
-    class Admin:
-        pass
-    
-    def __unicode__(self):
-        return self.nombre
-    
-    def get_absolute_url(self):
-        return "cursos/%s/" % self.codigo 
-
-
-class Recurso(models.Model):
-    """
-    Un recurso es la unidad mínima de contenido de una lección. Es una secuencia de código HTML y puede contener un archivo adjunto.
-    """
-    TIPOS_RECURSO = (
-        ('VID', 'Video'),
-        ('AUD', 'Audio'),
-        ('IMG', 'Imagen'),
-        ('TEX', 'Texto'),
-        ('MULTI', 'Multimedia'),
-    )
-    #Atributos básicos y obligatorios
-    autor = models.ManyToManyField(User)
-    nombre = models.CharField(max_length=150, help_text="Ojo con la ortografía de los títulos en español. Máximo 150 caracteres.")
-    html = RichTextField(help_text="Contenido de la lección en HTML. Este es el texto que el usuario podrá ver y el profesor agregar automáticamente a su lección.")
-    tipo = models.CharField(max_length=5, choices=TIPOS_RECURSO)
-    #Atributos opcionales
-    adjunto = models.FileField(upload_to='resources', blank=True, help_text="Adjunto que se guardará con el recurso, como una imágen, PDF, video, etc.")
-    url = models.URLField(blank=True, help_text="URL del recurso si proviene de otro sitio, como Youtube, Slideshare, etc.")
-    #Meta datos opcionales
-    nombre_corto=models.CharField(max_length=30, blank=True, help_text="Nombre corto que se verá en listas y otros lugares. Debe ser legible. Máximo 30 caracteres.")
-    tags = TaggableManager()
-    
-    class Meta:
-        ordering=['nombre']
-        verbose_name_plural='Recursos'
-        
-    class Admin:
-        pass
-    
-    def __unicode__(self):
-        if self.nombre_corto:
-            return self.nombre_corto
-        return self.nombre
-    
-    def get_absolute_url(self):
-        return "recursos/%s/" % self.id
-
-
-class Leccion(models.Model):
-    """Una lección es la unidad de contenido de un curso. Representa una sección de una clase o curso que tiene unidad temática. Puede ser parte de una secuencia para conformar un curso o proyecto o ser independiente. Contiene recursos organizados, detallados, relacionados, etc.
-    """
-    cursos = models.ManyToManyField(Curso)
-    titulo = models.CharField(max_length=150)
-    autor = models.ManyToManyField(User)
-    fecha_publicacion = models.DateField()
-    fecha_actualizacion = models.DateField(auto_now=True)
-    recursos = models.ManyToManyField(Recurso)
-    html = models.TextField(blank=True)
-    tags = TaggableManager()
+#        return "cat/%s/" % self.slug
