d2660f00286a51218063788fcc92ba6952432659
Nando-bog <fernando.diazdelcastillo@gmail.com>
2014-01-12 17:50:30 -0500

    Primer lista de herramientas

--- studley/views.py
+++ studley/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from .models import Herramienta
 
-# Create your views here.
+
+class HerramientaListView(ListView):
+    model=Herramienta
+    context_object_name='herramientas'
+    template_name='herramientas_index.html'
+
