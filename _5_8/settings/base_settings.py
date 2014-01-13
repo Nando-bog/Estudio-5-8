85b7c89a6a4a1294469fde6eafb8b7f562d6ad65
Nando-bog <fernando.diazdelcastillo@gmail.com>
2014-01-12 22:58:37 -0500

    Static files for project level fixed

--- _5_8/settings/base_settings.py
+++ _5_8/settings/base_settings.py
@@ -82,12 +82,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+#STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '../static'),
+)
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, '../templates'),
