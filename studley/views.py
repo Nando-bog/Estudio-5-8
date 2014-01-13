b13648c14a8d3806a9ab72c5345585c4b33a2a8e
Nando-bog <fernando.diazdelcastillo@gmail.com>
2014-01-12 20:07:57 -0500

    Lista de herramientas base

--- studley/views.py
+++ studley/views.py
@@ -1,12 +1,18 @@
+#coding=utf-8
+#Views para el app Studley. Versión 0.1.
+
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-from .models import Herramienta
+from .models import HerramientaBase
 
 
 class HerramientaListView(ListView):
-    model=Herramienta
+    model=HerramientaBase
     context_object_name='herramientas'
     template_name='herramientas_index.html'
 
+
+class HerramientaDetailView(DetailView):
+    pass
