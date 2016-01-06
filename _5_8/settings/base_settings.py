83cbccea2f7a68ce7733ccb987c12f7a265977a5
Fernando Díaz del Castillo <fernando.diazdelcastillo+github@gmail.com>
2016-01-06 15:29:58 -0500

    Heroku
    Config changes to attempt Heroku deployment.

--- _5_8/settings/base_settings.py
+++ _5_8/settings/base_settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+#THIRD PARTY IMPORTS
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -106,3 +108,24 @@ USE_L10N = True
 USE_TZ = True
 
 
+#HEROKU CONFIGURATION
+# Parse database configuration from $DATABASE_URL
+DATABASES['default'] =  dj_database_url.config()
+# Enable Persistent Connections
+DATABASES['default']['CONN_MAX_AGE'] = 500
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- _5_8/wsgi.py
+++ _5_8/wsgi.py
@@ -6,11 +6,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
-
+#Python and Python standard imports
 import os
-
 from django.core.wsgi import get_wsgi_application
+#Third party imports
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "_5_8.settings")
 
 application = get_wsgi_application()
+
+application = DjangoWhiteNoise(application)
