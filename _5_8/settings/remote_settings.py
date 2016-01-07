179af21549fd073f20289edc22bf72c4f4e6e641
Fernando Díaz del Castillo <fernando.diazdelcastillo+github@gmail.com>
2016-01-07 13:09:18 -0500

    static fiddling

--- _5_8/settings/remote_settings.py
+++ _5_8/settings/remote_settings.py
@@ -23,8 +23,8 @@ DATABASES['default']['CONN_MAX_AGE'] = 500
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, '../static')
-#STATIC_ROOT = 'staticfiles'
+#STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 # STATICFILES_DIRS = (
