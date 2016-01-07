2f66ab642c532c73fccc48d521e337d07a8db04c
Fernando Díaz del Castillo <fernando.diazdelcastillo+github@gmail.com>
2016-01-06 22:18:48 -0500

    Fiddling for Heroku
    Media files are so darn hard to get to work in Python. I hate this.

--- _5_8/settings/remote_settings.py
+++ _5_8/settings/remote_settings.py
@@ -12,7 +12,8 @@ SECRET_KEY = 't-5s+-dalv%cd1nk_!-l*y8i2nr7nwdm#!b#q3=9p23snk_!qr'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 STATIC_URL = '/static/'
 
