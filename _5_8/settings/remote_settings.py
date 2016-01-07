32a2bde83a07da388d0cf3930ecccd86853292cf
Fernando Díaz del Castillo <fernando.diazdelcastillo+github@gmail.com>
2016-01-06 22:04:02 -0500

    Fiddling

--- _5_8/settings/remote_settings.py
+++ _5_8/settings/remote_settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 't-5s+-dalv%cd1nk_!-l*y8i2nr7nwdm#!b#q3=9p23snk_!qr'
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
