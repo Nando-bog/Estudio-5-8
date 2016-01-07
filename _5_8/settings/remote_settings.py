52bee9c732a0da056dedd6e4f62a9320b9609f8c
Fernando Díaz del Castillo <fernando.diazdelcastillo+github@gmail.com>
2016-01-06 22:21:56 -0500

    DB

--- _5_8/settings/remote_settings.py
+++ _5_8/settings/remote_settings.py
@@ -8,6 +8,14 @@ DEBUG = True
 
 SECRET_KEY = 't-5s+-dalv%cd1nk_!-l*y8i2nr7nwdm#!b#q3=9p23snk_!qr'
 
+##HEROKU DATABASE
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Enable Persistent Connections
+DATABASES['default']['CONN_MAX_AGE'] = 500
+
 ### HEROKU ###
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
