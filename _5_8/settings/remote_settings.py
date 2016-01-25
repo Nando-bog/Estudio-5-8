4ae26a5cb0c5fc2cb7c6f050e73c5d04619e2496
Fernando Díaz del Castillo <fernando.diazdelcastillo+github@gmail.com>
2016-01-24 23:20:36 -0500

    TLS

--- _5_8/settings/remote_settings.py
+++ _5_8/settings/remote_settings.py
@@ -60,7 +60,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
-EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
